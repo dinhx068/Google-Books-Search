f638af5066ec3bcf8f5127145b07ce5821494cb6
Joe <dinhx068@umn.edu>
2018-11-28 19:54:20 -0600

    Don't think these need to be required

--- models/book.js
+++ models/book.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const googlebooks = new Schema({
-  authors: { type: [String], required: true },
-  description: { type: String, required: true },
-  image: { type: String, required: true },
-  link: { type: String, required: true },
+  authors: { type: [String] },
+  description: { type: String },
+  image: { type: String },
+  link: { type: String },
   title: { type: String, required: true }
 });
 
